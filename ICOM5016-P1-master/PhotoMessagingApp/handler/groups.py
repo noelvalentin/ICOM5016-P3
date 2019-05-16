ebb9a10c15ec4796670b0fb74bc9e0012dc16968
Jean C. Merced <jean.merced2@upr.edu>
2019-05-16 11:21:23 -0400

    Update groups.py

--- ICOM5016-P1-master/PhotoMessagingApp/handler/groups.py
+++ ICOM5016-P1-master/PhotoMessagingApp/handler/groups.py
@@ -1,5 +1,6 @@
 from flask import jsonify
-from dao.groups import GroupsDAO
+from src.dao.groups import GroupsDAO
+
 
 class GroupHandler:
 
@@ -8,7 +9,7 @@ class GroupHandler:
         result['uid'] = row[0]
         result['gid'] = row[1]
         result['gname'] = row[2]
-        result['first_name']=row[3]
+        result['first_name'] = row[3]
         return result
 
 #--------------- Phase 2 ---------------#
@@ -20,19 +21,19 @@ class GroupHandler:
         for row in group_list:
             result = self.build_group_chat_dict(row)
             result_list.append(result)
-        return jsonify(Groups = result_list)
+        return jsonify(Groups=result_list)
 
 #----------------- END -----------------#
 
     def createGroup(self, form):
         print("form: ", form)
         if len(form) != 2:
-            return jsonify(Error = "Malformed post request"), 400
+            return jsonify(Error="Malformed post request"), 400
         else:
             gname = form['gname']
             ownerID = form['ownerID']
 
-            if gname and ownerID :
+            if gname and ownerID:
                 dao = GroupsDAO()
                 gid = dao.createGroup(gname, ownerID)
                 #result = self.build_user_info_dict(uid, first_name,  last_name, phone, email)
@@ -40,52 +41,64 @@ class GroupHandler:
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
 
-'''
-    def getGroupById(self, gid):
-        dao = GroupsDAO()
-        row = dao.getGroupById(gid)
-        if not row:
-            return jsonify(Error = "Group Not Found"), 404
+    def postToGroup(self, form):
+        print("form: ", form)
+        if len(form) < 0:
+            return jsonify(Error="Malformed post request"), 400
         else:
-            group = self.build_group_chat_dict(row)
-            return jsonify(Group = group)
+            image = form['image']
+            text = form['text']
+            uid = form['uid']
+            gid = form['gid']
+            oid = form['oid']
 
-    def getGroupByGroupName(self, gname):
+            if text and uid:
+                dao = GroupsDAO()
+                result = dao.postToGroup(image, text, uid, gid, oid)
+                # result = self.build_user_info_dict(uid, first_name,  last_name, phone, email)
+                return jsonify(User=result), 201
+            else:
+                return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def getMyGroups(self, uid):
         dao = GroupsDAO()
-        group_list = dao.getGroupByGroupName(gname)
+
+        group_list = dao.getMyGroups(uid)
         result_list = []
         for row in group_list:
             result = self.build_group_chat_dict(row)
             result_list.append(result)
-        return jsonify(Groups = result_list)
+        return jsonify(Groups=result_list)
 
-    def getAllGroupsByOwnerID(self, ownerID):
+    def deleteMember(self, gid, uid):
         dao = GroupsDAO()
-        group_list = dao.getAllGroupsByOwnerID(ownerID)
-        result_list = []
-        for row in group_list:
-            result = self.build_group_dict(row)
-            result_list.append(result)
-        return jsonify(Groups = result_list)
-'''
-
-'''
-    def createGroup(self):
-        dao = GroupsDAO()
-        result = dao.createGroup()
-        return result
+        # if not dao.isMember(gid,uid):
+        #   return jsonify(Error="Part not found."), 404
+        # else:
+        dao.deleteMember(gid, uid)
+        return jsonify(DeleteStatus="OK"), 200
 
     def deleteGroup(self, gid):
         dao = GroupsDAO()
-        if not dao.getGroupById(gid):
-            return jsonify(Error="Group not found."), 404
-        result = dao.deleteGroup(gid)
-        return result
+        # if not dao.isMember(gid,uid):
+        #   return jsonify(Error="Part not found."), 404
+        # else:
+        dao.deleteGroup(gid)
+        return jsonify(DeleteStatus="OK"), 200
 
-    def updateGroup(self, gid):
-        dao = GroupsDAO()
-        if not dao.getGroupById(gid):
-            return jsonify(Error="Group not found."), 404
-        result = dao.updateGroup(gid)
-        return result
-'''
+    def addMember(self, form):
+        print("form: ", form)
+        if len(form) < 0:
+            return jsonify(Error="Malformed post request"), 400
+        else:
+            gid = form['gid']
+            uid = form['uid']
+
+            if gid and uid:
+                dao = GroupsDAO()
+                dao.addMember(uid, gid)
+                return jsonify(User="OK"), 200
+            else:
+                return jsonify(Error="Unexpected attributes in post request"), 400
+
+
