043226a46b5fecef6bb3ba859a249dd7bbcc327f
Bernardo2207 <33329311+Bernardo2207@users.noreply.github.com>
2019-05-16 13:58:21 -0400

    .

--- ICOM5016-P1-master/PhotoMessagingApp/handler/messages.py
+++ ICOM5016-P1-master/PhotoMessagingApp/handler/messages.py
@@ -1,7 +1,6 @@
 from flask import jsonify
 from dao.messages import MessagesDAO
 
-
 class MessageHandler:
 
     def build_message_dict(self, row):
@@ -20,28 +19,28 @@ class MessageHandler:
         result['number_of_reactions'] = row[0]
         return result
 
-    def build_hashtag_dict(self, row):
-        result = {}
+    def build_hashtag_dict(self,row):
+        result ={}
         result['tag'] = row[0]
-        result['count'] = row[1]
+        result['count']=row[1]
         return result
 
-    def build_stats_dict(self, row):
-        result = {}
+    def build_stats_dict(self,row):
+        result ={}
         result['date'] = row[0]
-        result['count'] = row[1]
+        result['count']=row[1]
         return result
 
 #--------------- Phase 2 ---------------#
 
-    def getAllMessages(self, oid):
+    def getAllMessages(self,oid):
         dao = MessagesDAO()
         messages_list = dao.getAllMessages(oid)
         result_list = []
         for row in messages_list:
             result = self.build_message_dict(row)
             result_list.append(result)
-        return jsonify(Messages=result_list)
+        return jsonify(Messages = result_list)
 
     def getMessagesByGroupId(self, gid):
         dao = MessagesDAO()
@@ -50,25 +49,25 @@ class MessageHandler:
         for row in messages_list:
             result = self.build_message_dict(row)
             result_list.append(result)
-        return jsonify(Messages=result_list)
+        return jsonify(Messages = result_list)
 
     def getLikesByMessageId(self, mid):
         dao = MessagesDAO()
         row = dao.getLikesByMessageId(mid)
         if not row:
-            return jsonify(Error="Message Not Found"), 404
+            return jsonify(Error = "Message Not Found"), 404
         else:
-            likes = self.build_message_reaction_dict(row)
-            return jsonify(Likes=likes)
+            likes= self.build_message_reaction_dict(row)
+            return jsonify(Likes = likes)
 
     def getDislikesByMessageId(self, mid):
         dao = MessagesDAO()
         row = dao.getDislikesByMessageId(mid)
         if not row:
-            return jsonify(Error="Message Not Found"), 404
+            return jsonify(Error = "Message Not Found"), 404
         else:
             dislikes = self.build_message_reaction_dict(row)
-            return jsonify(Dislikes=dislikes)
+            return jsonify(Dislikes = dislikes)
 
     def getPostsCount(self):
         dao = MessagesDAO()
@@ -115,6 +114,7 @@ class MessageHandler:
             result_list.append(result)
         return jsonify(Messages=result_list)
 
+
     def likeMessage(self, form):
         print("form: ", form)
         if len(form) < 0:
@@ -124,11 +124,12 @@ class MessageHandler:
             mid = form['mid']
 
             if uid and mid:
-                dao = MessagesDAO()
-                dao.likeMessage(uid, mid)
+                dao =MessagesDAO()
+                dao.likeMessage(uid,mid)
                 return jsonify(User="OK"), 200
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
+
 
     def dislikeMessage(self, form):
         print("form: ", form)
@@ -145,17 +146,99 @@ class MessageHandler:
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
 
+
     def createHashtag(self, form):
         print("form: ", form)
         if len(form) < 0:
             return jsonify(Error="Malformed post request"), 400
         else:
-            tag = form['tag']
+            tag = form['tag'];
 
-            if tag:
+
+            if tag :
                 dao = MessagesDAO()
                 dao.createHashtag(tag)
                 return jsonify(User="OK"), 200
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
+
+    def numReplies(self, oid):
+        dao = MessagesDAO()
+        row = dao.numReplies(oid)
+        if not row:
+            return jsonify(OK="Message Not Found")
+        else:
+            dislikes = self.build_message_reaction_dict(row)
+            return jsonify(Dislikes=dislikes)
+
 #----------------- END -----------------#
+
+'''
+    def getAllMessagesByUser(self, posterid):
+        dao = MessagesDAO()
+        messages_list = dao.getAllMessagesByUser(posterid)
+        result_list = []
+        for row in messages_list:
+            result = self.build_message_dict(row)
+            result_list.append(result)
+        return jsonify(Messsages = result_list)
+
+    def getMessageById(self, mid):
+        dao = MessagesDAO()
+        row = dao.getMessageById(mid)
+        if not row:
+            return jsonify(Error = "Message Not Found"), 404
+        else:
+            message = self.build_message_dict(row)
+            return jsonify(Message = message)
+
+    def getMessagesByDate(self, date):
+        dao = MessagesDAO()
+        messages_list = dao.getMessagesByDate(date)
+        result_list = []
+        for row in messages_list:
+            result = self.build_message_dict(row)
+            result_list.append(result)
+        return jsonify(Messsages = result_list)
+
+
+    def getTrendingTopics(self):
+        dao = MessagesDAO()
+        result = dao.getTrendingTopics()
+        return result
+
+    def getMessagesPerDayByUser(self, posterid, date):
+        dao = MessagesDAO()
+        messages_list = dao.getMessagesPerDayByUser(posterid, date)
+        result_list = []
+        for row in messages_list:
+            result = self.build_message_dict(row)
+            result_list.append(result)
+        return jsonify(Messsages = result_list)
+
+    def getActiveUsers(self):
+        dao = MessagesDAO()
+        result = dao.getActiveUsers()
+        return result
+'''
+
+'''
+    def createMessage(self):
+        dao = MessagesDAO()
+        result = dao.createMessage()
+        return result
+
+    def deleteMessage(self, mid):
+        dao = MessagesDAO()
+        if not dao.getMessageById(mid):
+            return jsonify(Error = "Message Not Found"), 404
+        result = dao.deleteMessage(mid)
+        return result
+
+    def updateMessage(self, mid):
+        dao = MessagesDAO()
+        if not dao.getMessageById(mid):
+            return jsonify(Error = "Message Not Found"), 404
+        result = dao.updateMessage(mid)
+        return result
+'''
